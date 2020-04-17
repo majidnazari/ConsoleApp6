2408802fc45057911bac7150e0ab33fb758d0644
majid <majid@DESKTOP-US64O5C>
2020-04-17 11:10:43 +0430

    XML NODE

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace ConsoleApp6
 {
     class Program
     {
+        
+
         public class person
         {
             public string name { get; set; }
@@ -13,8 +17,44 @@ namespace ConsoleApp6
         };
         static void Main(string[] args)
         {
+            XmlDocument doc = new XmlDocument();
+            XmlDocument doc2 = new XmlDocument();
+            doc.Load(@"e:\g2.txt");
 
            
+
+            // XmlNode node = doc.DocumentElement.SelectSingleNode("/catalog/book");
+
+            XmlNodeList nodes = doc.DocumentElement.SelectNodes("/catalog/book");
+            foreach (XmlNode node in nodes)
+            {
+                for(int i=0; i<2; i++)
+                {
+                   
+                    XmlNode anothernode = node.CloneNode(true);
+                    anothernode.SelectSingleNode("publish_date").InnerText = $"*GoTemp[{i}]_yy/mm/dd";
+                    doc.DocumentElement.InsertAfter(anothernode, node);
+                }
+                
+            }
+            
+            //doc.AppendChild(node);
+            //doc.DocumentElement.AppendChild(anothernode);
+           
+           
+            doc.Save(@"e:\g2.txt");
+            //doc.Load(@"e:\g2.txt");
+            //XmlNodeList nodes = doc.DocumentElement.SelectNodes("/catalog/book/price");
+            //
+            //foreach (XmlNode node_temp in nodes)
+            //{
+            //    node_temp.InnerText = "100";
+               
+                
+            //}
+            //doc.Save(@"e:\g2.txt");
+
+            XElement custOrd = XElement.Load("e:\\g1.txt");
             Console.ReadKey();
 
                 List<person> people = new List<person>
